d3e7e5f94ba07c712f94b4c002c7f121703dc511
orloffv <orloff.v@gmail.com>
2014-12-08 12:09:02 +0400

    add convertToSelect2Func

--- src/select2.js
+++ src/select2.js
@@ -72,10 +72,18 @@ angular.module('ui.select2', []).value('uiSelect2Config', {}).directive('uiSelec
             angular.forEach(
               angular_data,
               function(value, index) {
-                model.push({'id': value, 'text': value});
+                if(opts.convertToSelect2Func && (typeof (opts.convertToSelect2Func) === 'function')) {
+                  model.push(opts.convertToSelect2Func.call(this, value));
+                } else {
+                  model.push({'id': value, 'text': value});
+                }
               });
           } else {
-            model = angular_data;
+            if(opts.convertToSelect2Func && (typeof (opts.convertToSelect2Func) === 'function')) {
+              model = opts.convertToSelect2Func.call(this, angular_data);
+            } else {
+              model = angular_data;
+            }
           }
           return model;
         };
@@ -157,7 +165,7 @@ angular.module('ui.select2', []).value('uiSelect2Config', {}).directive('uiSelec
             // Set the view and model value and update the angular template manually for the ajax/multiple select2.
             elm.bind("change", function (e) {
               e.stopImmediatePropagation();
-              
+
               if (scope.$$phase || scope.$root.$$phase) {
                 return;
               }
